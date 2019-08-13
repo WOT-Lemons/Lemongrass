e0c752c64b801ca71cd609626de274aa87f9bdb1
Capt-Tom <tom.mcnulty@autodesk.com>
2019-08-12 23:23:35 -0400

    Add logging, format lap output

--- lemongrass.py
+++ lemongrass.py
@@ -4,7 +4,6 @@
 from __future__ import print_function, unicode_literals
 from PyInquirer import prompt, print_json
 from pprint import pprint
-from eprint import eprint
 import os
 import sys
 import requests
@@ -14,13 +13,15 @@ import time
 import signal
 import csv
 import pandas as pd
+import logging
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from oauth2client.client import flow_from_clientsecrets
 
-pd.set_option("display.max_rows", 1024)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+pd.set_option("display.max_rows", 1024)
 underline = "-" * 80
 
 def callRaceMonitor(endpoint, payload):
@@ -29,12 +30,14 @@ def callRaceMonitor(endpoint, payload):
     api_url = api_base_url + api_endpoint
     r = requests.post(api_url, data = payload)
     if r.status_code != 200:
-        print('{} - Possible rate limiting, waiting 60 seconds...'.format(r.status_code))
+        logging.error('{} - Possible rate limiting, waiting 60 seconds...'.format(r.status_code))
         time.sleep(60)
         r = requests.post(api_url, data = payload)
     return json.loads(r.text)
 
 def printRankings(sorted_competitors):
+    underline = "-" * 80
+    print(underline)
     list_of_names = []
 
     for competitor in sorted_competitors:
@@ -42,38 +45,26 @@ def printRankings(sorted_competitors):
             if item == "FirstName":
                 list_of_names.append(competitor[item])
 
-    print(f'{"Pos.": <4} {"Laps": <4} {"First Name":<32} {"Competitor ID":<15} {"Transponder ID":<6}')
+    print(f'{"Pos.": <4} {"#":<4} {"First Name": <32} {"Laps": <4} {"Competitor ID":<15} {"Transponder ID":<6}')
     underline = "-" * 80
     print(underline)
 
     for competitor in sorted_competitors:
         #print(competitor['Position'], competitor['Laps'], competitor['FirstName'], competitor['ID'], competitor['Transponder'])
-        print(f"{competitor['Position']: <4} {competitor['Laps']: <4} {competitor['FirstName']:<32} {competitor['ID']:<15} {competitor['Transponder']:<6}")
+        print(f"{competitor['Position']: <4} {competitor['Number']:<4} {competitor['FirstName']:<32} {competitor['Laps']: <4} {competitor['ID']:<15} {competitor['Transponder']:<6}")
         
     print(underline)
     return
 
-def printLapTimes(competitor_lap_times):
-
-    return
-
 creds = None
-
-#parser = argparse.ArgumentParser(description='Process some integers.')
-#parser.add_argument('--raceid', dest='race_id', action='store',
-#                    help='Race ID from https://www.race-monitor.com/Live/Race/')
-
-#args = parser.parse_args(['--raceid'])
-#print(args)
-#race_id = args[]
 
 if os.path.exists('./.token'):
     f = open('.token', 'r')
     token = f.readline().rstrip()
     if token != "":
-        eprint("SETUP: Tokenfile opened and read")
+        logging.debug("Tokenfile opened and read")
 else:
-    eprint("ERROR: Didn't open ./.token")
+    logging.error("Didn't open ./.token")
     sys.exit("ERROR: Didn't open ./.token")
 
 while True:
@@ -89,14 +80,14 @@ while True:
     try: race_id
     except NameError: x = None
 
-eprint("INFO: Getting details for {}".format(race_id))
+logging.debug("Getting details for {}".format(race_id))
 payload = { 'apiToken': token, 'raceID': race_id}
 race_details = callRaceMonitor('/v2/Race/RaceDetails', payload)
 
 series_id = race_details['Race']['SeriesID']
 race_type_id = race_details['Race']['RaceTypeID']
 
-eprint("INFO: Getting sessions for {}".format(race_id))
+logging.debug("Getting sessions for {}".format(race_id))
 payload = { 'apiToken': token, 'raceID': race_id}
 sessions_for_race = callRaceMonitor('/v2/Results/SessionsForRace', payload)
 
@@ -105,10 +96,10 @@ session_ids_for_race = []
 for i in sessions_for_race['Sessions']:
     session_ids_for_race.append(i['ID'])
 
-eprint("INFO: Race {} has {} sessions, {}".format(race_id, len(session_ids_for_race), session_ids_for_race))
+logging.debug("Race {} has {} sessions, {}".format(race_id, len(session_ids_for_race), session_ids_for_race))
 
 last_session_id = sessions_for_race['Sessions'][-1]['ID']
-eprint("INFO: Getting rankings from last session, {}.".format(sessions_for_race['Sessions'][-1]['ID']))
+logging.debug("Getting rankings from last session, {}.".format(sessions_for_race['Sessions'][-1]['ID']))
 payload = { 'apiToken': token, 'sessionID': last_session_id}
 last_session_details = callRaceMonitor('/v2/Results/SessionDetails', payload)
 
@@ -119,33 +110,33 @@ printRankings(sorted_competitors)
 
 while True:
     try: 
-        transponder_id = sys.argv[2]
+        car_number = sys.argv[2]
         break
     except IndexError:
-        transponder_id = input("Transponder ID: ")
+        car_number = input("Car Number: ")
         break
 
 competitor_details = []
 competitor_lap_times = []
 
 for session_id in session_ids_for_race:
-    eprint("INFO: Getting session details for {} including lap times.".format(session_id))
+    logging.debug("Getting session details for {} including lap times.".format(session_id))
     payload = { 'apiToken': token, 'sessionID': session_id, 'includeLapTimes': True}
     lap_times = callRaceMonitor('/v2/Results/SessionDetails', payload)
 
     for competitor in lap_times['Session']['SortedCompetitors']:
-        if competitor['Transponder'] == transponder_id:
+        if competitor['Number'] == car_number:
             competitor_lap_times = competitor_lap_times + competitor['LapTimes']
             if session_id == last_session_id:
                 competitor_details = competitor
 
-print(underline)
-print("\nTeam: {:<32} Transponder: {:<4}".format(competitor_details['FirstName'], competitor_details['Transponder']))
+print("\nTeam: {:<6} Car Number: {:<4} Transponder: {}".format(competitor_details['FirstName'], competitor_details['Number'], competitor_details['Transponder']))
 print("Best Position:\t{:>}\nFinal Position:\t{:>}\nTotal Laps:\t{:>}\nBest Lap:\t{:>}\nBest Lap Time:\t{:>}\nTotal Time:\t{:>}".format(competitor_details['BestPosition'],competitor_details['Position'], competitor_details['Laps'], competitor_details['BestLap'], competitor_details['BestLapTime'], competitor_details['TotalTime']))
 print(underline)
 
 filename = "{}-{}-{}".format(competitor_details['FirstName'], race_id, last_session_id)
 print("Saving lap times to {}.csv".format(filename))
+print(underline)
 lap_csv_fh = open('./%s.csv' % filename, 'w')
 csvwriter = csv.writer(lap_csv_fh)
 
@@ -162,7 +153,9 @@ lap_csv_fh.close()
 
 #printLapTimes(competitor_lap_times)
 
-print(pd.io.json.json_normalize(competitor_lap_times))
+lap_time_df = pd.io.json.json_normalize(competitor_lap_times)
+
+print(lap_time_df.to_string(index=False))
 
 
 """
