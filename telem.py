b130e2ccbd6a329102fe4989383cb810dcc0f9a4
TLMcNulty <tom@mcnulty.in>
2020-01-26 05:19:00 +0000

    Auto scan for supported commands, add heartbeat

--- telem.py
+++ telem.py
@@ -10,13 +10,16 @@ from time import sleep
 import obd
 import datetime
 
+
 def new_value(r):
+    #print(r)
     ts = datetime.datetime.now()
     measurement = str(r.command).split(":")[1]
     measurement = measurement.replace(" ", "-")
-    print measurement
+    #print measurement
     try:
-        print r.value.magnitude
+        if r.value.magnitude is None:
+            r.value.magnitude = 0
         json_body = [
         {
             "measurement": measurement,
@@ -26,8 +29,8 @@ def new_value(r):
             }
         }]
     except AttributeError:
-        print r.value[1]
-
+        if r.value[1] is None:
+            r.value[1] = 0
         json_body = [
         {
             "measurement": measurement,
@@ -52,7 +55,9 @@ def new_fuel_status(r):
     elif "Open loop due to system failure" in r.value:
         fuel_status = 3
     elif "Closed loop, using at least one oxygen sensor but there is a fault in the feedback system" in r.value:
-        fuel_status =4
+        fuel_status = 4
+    if fuel_status is None:
+        fuel_status = 255
     json_body = [
     {
         "measurement": measurement,
@@ -63,30 +68,47 @@ def new_fuel_status(r):
     }]
     client.write_points(json_body)
 
-obd.logger.setLevel(obd.logging.DEBUG)
-connection = obd.Async()
+#obd.logger.setLevel(obd.logging.INFO)
 
-connection.watch(obd.commands.FUEL_STATUS, callback=new_fuel_status )
-connection.watch(obd.commands.ENGINE_LOAD, callback=new_value )
-connection.watch(obd.commands.COOLANT_TEMP, callback=new_value )
-connection.watch(obd.commands.SHORT_FUEL_TRIM_1, callback=new_value )
-connection.watch(obd.commands.LONG_FUEL_TRIM_1, callback=new_value )
-connection.watch(obd.commands.FUEL_PRESSURE, callback=new_value )
-connection.watch(obd.commands.INTAKE_PRESSURE, callback=new_value )
-connection.watch(obd.commands.RPM, callback=new_value )
-connection.watch(obd.commands.SPEED, callback=new_value )
-connection.watch(obd.commands.TIMING_ADVANCE, callback=new_value )
-connection.watch(obd.commands.INTAKE_TEMP, callback=new_value )
-connection.watch(obd.commands.MAF, callback=new_value )
-connection.watch(obd.commands.THROTTLE_POS, callback=new_value )
-connection.watch(obd.commands.O2_B1S1, callback=new_value )
-connection.watch(obd.commands.O2_B1S2, callback=new_value )
+connection = obd.Async()
+supported_commands = connection.supported_commands
+watch_commands = {}
+
+for command in supported_commands:
+    if "DTC" not in command.name:
+        if "MIDS" not in command.name:
+            if "PIDS" not in command.name:
+                if "O2_SENSORS" not in command.name:
+                    if command.name != "STATUS":
+                        if "ELM" not in command.name:
+                            if "OBD" not in command.name:
+                                if "FUEL_STATUS" in command.name:
+                                    print(command.name, " supported, watching...")
+                                    connection.watch(command, callback=new_fuel_status)
+                                else:
+                                    print(command.name, " supported, watching...")
+                                    connection.watch(command, callback=new_value)
+
 connection.start()
 
-obd.logger.setLevel(obd.logging.INFO)
+#obd.logger.setLevel(obd.logging.INFO)
 
 while True:
     sleep(0.05)
+    
+    ts = datetime.datetime.now()
+    measurement = "heartbeat"
+
+    json_body = [
+        {
+            "measurement": measurement,
+            "time": ts,
+            "fields": {
+                "value": 1
+            }
+        }]
+
+    client.write_points(json_body)
 #except Exception as e:
 #    print(e)
 #    connection.close()
